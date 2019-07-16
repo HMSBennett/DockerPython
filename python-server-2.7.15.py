24375f06ee58d474361a2dac35c1f95297fbafb9
HMSBennett <harrisonsbennett@gmail.com>
2019-07-16 09:28:42 +0100

    fixed version issues and spelling mistakes

--- python-server-2.7.15.py
+++
@@ -1,7 +0,0 @@
-import SimpleHTTPServer
-import SockerServer
-
-PORT = 9000
-Handler = SimpleGTTPServer.SimpleHTTPRequestHandler
-httpd = SockerServer.TCPServer(("", PORT), Handler)
-https.serve_forever()
